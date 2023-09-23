3abee83b2c1d01aa9d1f5d86deb8aca3a5fb48b1
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-24 00:39:26 +0100

    insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,40 +7,39 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *lists, *head, *current, *tail;
-    int i, j;
+	listint_t *lists, *head, *current, *tail;
 
-    lists = *list;
-    if (lists == NULL || lists->next == NULL || lists->prev == NULL)
-    {
-        print_list(lists);
-    }
+	lists = *list;
+	if (lists == NULL || lists->next == NULL || lists->prev == NULL)
+	{
+		print_list(lists);
+	}
 
-    head = NULL;
-    current = lists;
-    while (lists != NULL)
-    {
-        lists = lists->next;
-        if (head == NULL || current->n < head->n)
-        {
-            current->next = head;
-            head = current;
-            print_list(head);
-        }
-        else
-        {
-            tail = head;
-            while (tail != NULL)
-            {
-                if (tail->next == NULL || current->n < tail->n)
-                {
-                    current->next = tail->next;
-                    tail->next =  current;
-                    print_list(head);
-                    break;
-                }
-                tail = tail->next;
-            }
-        }
-    }
+	head = NULL;
+	current = lists;
+	while (lists != NULL)
+	{
+		lists = lists->next;
+		if (head == NULL || current->n < head->n)
+		{
+			current->next = head;
+			head = current;
+			print_list(head);
+		}
+		else
+		{
+			tail = head;
+			while (tail != NULL)
+			{
+				if (tail->next == NULL || current->n < tail->n)
+				{
+					current->next = tail->next;
+					tail->next =  current;
+					print_list(head);
+					break;
+				}
+				tail = tail->next;
+			}
+		}
+	}
 }
