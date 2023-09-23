cb0ac6ced9f1f531649b82bc989c52c37e9f7132
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-23 22:53:17 +0100

    betty

--- print_list.c
+++ print_list.c
@@ -9,12 +9,14 @@
 void print_list(const listint_t *list)
 {
     int i;
-
+    
     i = 0;
     while (list)
     {
         if (i > 0)
+        {
             printf(", ");
+        }  
         printf("%d", list->n);
         ++i;
         list = list->next;
