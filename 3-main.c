1826d3e49caa74c48a8e63a942a4871d626fe4da
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-25 19:46:43 +0100

    quick

--- 3-main.c
+++ 3-main.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    int array[] = {2, 8, 16, 0, 93, 48, 72, -88, 93, 96};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     print_array(array, n);
